44d99000c7b2e60ee54da26efb75f256d53f1e2e
Greg Karlin <gregory.l.karlin@gmail.com>
2014-12-02 11:07:15 -0500

    moved interval duration to config

--- app/actors/TrafficMonitor.scala
+++ app/actors/TrafficMonitor.scala
@@ -2,6 +2,7 @@ package actors
 
 import akka.actor.{Props, Actor}
 import scala.concurrent.duration._
+import play.api.Play.current
 
 object TrafficMonitor {
   case class Start()
@@ -15,12 +16,13 @@ class TrafficMonitor extends Actor {
   import TrafficMonitor._
   //implicit val context = scala.concurrent.ExecutionContext.Implicits.global
   val trafficClient = context.actorOf(Props[TrafficClient])
+  val intervalMS = current.configuration.getLong("monitor.interval").get
   import context.dispatcher
 
   def receive = {
 
     case Start() =>
-      context.system.scheduler.schedule(0 milliseconds, 5000 milliseconds, trafficClient, TrafficClient.GetHosts())
+      context.system.scheduler.schedule(0 milliseconds, intervalMS milliseconds, trafficClient, TrafficClient.GetHosts())
 
     case TrafficClient.Hosts(hosts) =>
       hosts.map( host => trafficClient ! TrafficClient.GetHostTraffic(host))
